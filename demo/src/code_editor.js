38eaa8ecd538c4083ecc49974eac9fbb0183c351
icebox246 <iceboxpoczta@gmail.com>
2023-09-11 18:36:13 +0200

    Move default code editor text to a separate text file

--- demo/src/code_editor.js
+++ demo/src/code_editor.js
@@ -1,30 +1,4 @@
-const defaultText = `// make run visible outside the app
-export run;
-
-// logs int value to output
-extern log_int := fn x: i32;
-// asks user for int value
-extern get_int := fn default: i32 -> i32;
-
-// this is the entry point of the app
-run := fn {
-    // add 1 and 2, and store the result in res
-    res := i32 add(1, 2);
-    log_int(res);
-
-    // uncomment line below to see interactive example
-    //interactive_doubling();
-};
-
-add := fn a: i32, b: i32 -> i32 {
-    return a + b;
-};
-
-interactive_doubling := fn {
-    num := i32;
-    num = get_int(21);
-    log_int(2 * num);
-};`
+import defaultText from './example.u?raw'
 
 /**
  * @type {(element: HTMLTextAreaElement)}
