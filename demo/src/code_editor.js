cef3fd63234173a2a1f623673625077363705543
icebox246 <iceboxpoczta@gmail.com>
2023-09-11 19:01:35 +0200

    Handle Tab pressing in code editor

--- demo/src/code_editor.js
+++ demo/src/code_editor.js
@@ -9,6 +9,29 @@ export function setupCodeEditor(element) {
     element.wrap = "off";
     element.value = defaultText;
 
+    element.addEventListener("keydown", (e) => {
+        if (e.key === "Tab") {
+            e.preventDefault();
+            const start = element.selectionStart;
+            const end = element.selectionEnd;
+            const indent = "  "
+            if (!e.shiftKey) {
+
+                element.value = element.value.substring(0, start)
+                    + indent + element.value.substring(end);
+
+                element.selectionStart = start + indent.length;
+                element.selectionEnd = start + indent.length;
+            } else {
+                if (start < indent.length) return;
+                element.value = element.value.substring(0, start - indent.length) + element.value.substring(end);
+                element.selectionStart = start - indent.length;
+                element.selectionEnd = start - indent.length;
+            }
+        }
+        console.log(e.key)
+    });
+
     return {
         getSourceCode() {
             return element.value;
